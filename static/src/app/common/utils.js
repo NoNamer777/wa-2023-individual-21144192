aef7bbacebf69c1e4f8a476c02574cdcc6f7d79b
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-10 16:22:51 +0100

    Add fetching an JSON file

--- static/src/app/common/utils.js
+++ static/src/app/common/utils.js
@@ -1,11 +1,18 @@
 const parser = new DOMParser();
 
+/** Fetches an HTML file and extracts the contents of it. */
 async function fetchTemplate(location) {
     const template = await (await fetch(`${location}.html`)).text();
 
     return parser.parseFromString(template, 'text/html').body.firstChild;
 }
 
+/** Fetches a JSON file. */
+async function fetchJsonFile(location) {
+    return await (await fetch(`${location}.json`)).json();
+}
+
+/** Capitalizes a string. */
 function capitalize(input) {
     return input.slice(0, 1).toUpperCase() + input.slice(1);
 }
