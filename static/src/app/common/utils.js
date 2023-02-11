2d5edc4c88bfbe6b24fe6dd5237dde25422a88b3
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-11 11:28:48 +0100

    Run Prettier

--- static/src/app/common/utils.js
+++ static/src/app/common/utils.js
@@ -55,7 +55,5 @@ function getQueryParams() {
 function getQueryParam(param) {
     const queryParams = getQueryParams();
 
-    return JSON.stringify(queryParams) === '{}'
-        ? undefined
-        : queryParams[param];
+    return JSON.stringify(queryParams) === '{}' ? undefined : queryParams[param];
 }
