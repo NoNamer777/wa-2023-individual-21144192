0ed8a63ab5023cb720c5f09c56c77a1f0b482c3a
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-11 11:29:24 +0100

    Populate the overview with the cards for Races

--- static/src/app/pages/overview/overview.controller.js
+++ static/src/app/pages/overview/overview.controller.js
@@ -1,6 +1,35 @@
 function overviewController() {
+    const maxCardsPerPage = 5;
+    let pageNumber;
+    let template;
+
+    /** Reads the pageNumber query param in the route. */
+    function initializePageNumber() {
+        const pageNumberQueryParam = getQueryParam('pageNumber');
+
+        pageNumber = pageNumberQueryParam ? parseInt(pageNumberQueryParam) : 1;
+    }
+
+    /**
+     * Fetches a specific slice of data of Races, depending on the pageNumber
+     * and creates and inserts cards in the overview with that data.
+     */
+    async function initializeCards() {
+        await raceService.initialized;
+
+        const races = raceService.fetchPaginatedRaces(maxCardsPerPage, pageNumber);
+
+        for (const race of races) {
+            raceCardController(race);
+        }
+    }
+
+    /** Initializes the Overview page. */
     async function initialize() {
-        const template = await fetchTemplate(`app/pages/overview/${PAGES.overview}`);
+        template = await fetchTemplate(`app/pages/overview/${PAGES.overview}`);
+
+        initializePageNumber();
+        initializeCards();
 
         document.querySelector('article').replaceWith(template);
     }
