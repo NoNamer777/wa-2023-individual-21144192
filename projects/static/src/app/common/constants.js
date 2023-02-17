a9a07f8fa5ad50f0e973ce0681c7bce936f84414
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-17 14:17:26 +0100

    Add constants for sorting direction and size

--- projects/static/src/app/common/constants.js
+++ projects/static/src/app/common/constants.js
@@ -2,4 +2,11 @@ const PAGES = {
     overview: 'overview',
 };
 
+const SIZES = ['Tiny', 'Small', 'Medium', 'Large', 'Huge', 'Gargantuan'];
+
+const SORTING_DIRECTIONS = {
+    ascending: 'ASC',
+    descending: 'DESC',
+};
+
 const APP_TITLE_PREFIX = 'DnD 5e Races - ';
