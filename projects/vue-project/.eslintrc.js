2bc212581ebf07992554d44e7fd7d8a6a83cd2ba
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-18 00:01:04 +0100

    Change vue/eslintrc file extension

---
+++ projects/vue-project/.eslintrc.js
@@ -0,0 +1,14 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+    root: false,
+    extends: [
+        '../../.eslintrc.json',
+        'plugin:vue/vue3-essential',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+};
