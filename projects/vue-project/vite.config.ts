ea2f603317d8e7b6aa08d24902a5dc23ce70b08f
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-18 00:38:57 +0100

    Reorder vue-project structure

--- projects/vue-project/vite.config.ts
+++ projects/vue-project/vite.config.ts
@@ -1,15 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    assetsInclude: ['./favicon.ico'],
+    cacheDir: '../../../.vite',
+    plugins: [vue(), vueJsx()],
+    publicDir: './assets',
+    mode: 'production',
+    resolve: {
+        alias: {
+            '@vue-project': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    root: './src',
+});
