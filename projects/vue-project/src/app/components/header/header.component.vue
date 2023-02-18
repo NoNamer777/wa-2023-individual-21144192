025aed401733be86eb28a9d25acb784264413b81
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-18 13:12:18 +0100

    Add main styling for the header component

--- projects/vue-project/src/app/components/header/header.component.vue
+++ projects/vue-project/src/app/components/header/header.component.vue
@@ -1,3 +1,5 @@
 <template src="./header.component.html"></template>
 <style scoped lang="scss" src="./header.component.scss"></style>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
