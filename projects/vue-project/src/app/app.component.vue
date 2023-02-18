8bce4797bd7306729a8971386305e14c97baf0f5
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-18 12:07:55 +0100

    Add center component

--- projects/vue-project/src/app/app.component.vue
+++ projects/vue-project/src/app/app.component.vue
@@ -2,6 +2,7 @@
 <style scoped lang="scss" src="./app.component.scss"></style>
 
 <script setup lang="ts">
+import CenterComponent from './components/center/center.component.vue';
 import FooterComponent from './components/footer/footer.component.vue';
 import HeaderComponent from './components/header/header.component.vue';
 </script>
