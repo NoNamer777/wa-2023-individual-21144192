4741fb8a6996ec3b45a57e569d88e97b43cc067b
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-19 16:34:20 +0100

    Add PaginationComponent

---
+++ projects/vue-project/src/app/components/pagination/pagination.component.vue
@@ -0,0 +1,32 @@
+<template src="./pagination.component.html"></template>
+
+<script setup lang="ts">
+import { usePaginationStore } from '@vue-project/app/stores/pagination.store';
+import { computed, ref } from 'vue';
+
+const paginationStore = usePaginationStore();
+
+const numberOfPages = computed<number>(() => {
+    return paginationStore.getTotalNumberOfPages;
+});
+
+const isOnFirstPage = computed<boolean>(() => {
+    return paginationStore.getCurrentPage === 1;
+});
+
+const isOnLastPage = computed<boolean>(() => {
+    return paginationStore.getCurrentPage === paginationStore.getTotalNumberOfPages;
+});
+
+const previousPage = computed<number>(() => {
+    return isOnFirstPage.value ? 1 : paginationStore.getCurrentPage - 1;
+});
+
+const nextPage = computed<number>(() => {
+    return isOnLastPage.value ? paginationStore.getTotalNumberOfPages : paginationStore.getCurrentPage + 1;
+});
+
+function isOnPage(pageNumber: number): boolean {
+    return paginationStore.getCurrentPage === pageNumber;
+}
+</script>
