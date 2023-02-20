3ac7418ae87391bb0a6dfa17ca5931c0560a7bbe
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-20 21:53:04 +0100

    Patch query params from the pagination

--- projects/vue-project/src/app/components/header/pagination/pagination.component.vue
+++ projects/vue-project/src/app/components/header/pagination/pagination.component.vue
@@ -1,30 +1,22 @@
 <template src="./pagination.component.html" />
 
 <script setup lang="ts">
+import { type SortingAndFilteringQueryParams } from '@vue-project/app/models/pagination';
 import { usePaginationStore } from '@vue-project/app/stores/pagination.store';
 import { computed } from 'vue';
+import { useRoute } from 'vue-router';
 
+const route = useRoute();
 const paginationStore = usePaginationStore();
 
-const numberOfPages = computed<number>(() => {
-    return paginationStore.getTotalNumberOfPages;
-});
-
-const isOnFirstPage = computed<boolean>(() => {
-    return paginationStore.getCurrentPage === 1;
-});
-
-const isOnLastPage = computed<boolean>(() => {
-    return paginationStore.getCurrentPage === paginationStore.getTotalNumberOfPages;
-});
-
-const previousPage = computed<number>(() => {
-    return isOnFirstPage.value ? 1 : paginationStore.getCurrentPage - 1;
-});
-
-const nextPage = computed<number>(() => {
-    return isOnLastPage.value ? paginationStore.getTotalNumberOfPages : paginationStore.getCurrentPage + 1;
-});
+const numberOfPages = computed<number>(() => paginationStore.getTotalNumberOfPages);
+const isOnFirstPage = computed<boolean>(() => paginationStore.getCurrentPage === 1);
+const isOnLastPage = computed<boolean>(() => paginationStore.getCurrentPage === paginationStore.getTotalNumberOfPages);
+const previousPage = computed<number>(() => (isOnFirstPage.value ? 1 : paginationStore.getCurrentPage - 1));
+const nextPage = computed<number>(() =>
+    isOnLastPage.value ? paginationStore.getTotalNumberOfPages : paginationStore.getCurrentPage + 1
+);
+const queryParams = computed<SortingAndFilteringQueryParams>(() => route.query as SortingAndFilteringQueryParams);
 
 function isOnPage(pageNumber: number): boolean {
     return paginationStore.getCurrentPage === pageNumber;
