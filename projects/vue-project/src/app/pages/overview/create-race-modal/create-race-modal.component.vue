732684d7d1db99e39c333b737597ce0b68c7fa2b
Oscar Wellner <oscar.wellner@gmail.com>
2023-02-21 20:26:36 +0100

    Add modal for creating a new Race

---
+++ projects/vue-project/src/app/pages/overview/create-race-modal/create-race-modal.component.vue
@@ -0,0 +1,20 @@
+<template src="./create-race-modal.component.html"></template>
+
+<script setup lang="ts">
+import { type Race, sizeOrder, type Trait } from '@vue-project/app/models/race';
+import { useRaceStore } from '@vue-project/app/stores/race.store';
+import { ref } from 'vue';
+
+const newRace = ref<Race>({ traits: [] as Trait[] } as Race);
+const sizeOptions = ref<string[]>(Object.keys(sizeOrder));
+
+function onAddTrait(): void {
+    newRace.value.traits.push({ name: '', description: '' });
+}
+
+function onCreate(): void {
+    useRaceStore().addNewRace(newRace.value);
+
+    newRace.value = { traits: [] as Trait[] } as Race;
+}
+</script>
