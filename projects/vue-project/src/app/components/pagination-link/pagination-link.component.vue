7d0df868b842a0ee9bef1fe8917461a8c339eebb
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-07 11:06:29 +0100

    Extract pagination link related code from pagination

---
+++ projects/vue-project/src/app/components/pagination-link/pagination-link.component.vue
@@ -0,0 +1,32 @@
+<template>
+    <li class="page-item" :class="{ disabled: disabled, active: isOnPage(pageNumber) }">
+        <router-link
+            class="page-link"
+            :to="{ name: 'Overview', query: { ...queryParams, pageNumber: pageNumber } }"
+            active-class="_active"
+        >
+            {{ label }}
+        </router-link>
+    </li>
+</template>
+
+<script lang="ts" setup>
+import type { SortingFilteringQueryParams } from '@vue-project/app/models';
+import { usePaginationStore } from '@vue-project/app/stores';
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const props = withDefaults(defineProps<{ disabled: boolean; active: number; pageNumber: number; label: string }>(), {
+    disabled: false,
+    active: 1,
+});
+
+const queryParams = computed<SortingFilteringQueryParams>(() => useRoute().query as SortingFilteringQueryParams);
+
+function isOnPage(pageNumber: number): boolean {
+    if (isNaN(parseInt(props.label))) {
+        return false;
+    }
+    return usePaginationStore().getCurrentPage === pageNumber;
+}
+</script>
