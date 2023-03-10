27dea9c47be94182bb763dc4c76796a17f8bddb0
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-10 23:50:46 +0100

    Add controller for fetching race data

--- packages/nest-app/src/app/api/race/controllers/race.controller.ts
+++ packages/nest-app/src/app/api/race/controllers/race.controller.ts
@@ -1,9 +1,34 @@
-import { Controller } from '@nestjs/common';
-import { RaceService } from './race.service';
+import { Body, Controller, Delete, Get, HttpCode, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { RaceService } from '../services/race.service';
+import { CreateRaceData, Race } from '../models/race.model';
+import { ApiTags } from '@nestjs/swagger';
+import { HttpStatusCode } from 'axios';
 
+@ApiTags('api/race')
 @Controller({
     path: 'api/race',
 })
 export class RaceController {
-    constructor(private _raceService: RaceService) {}
+    constructor(private raceService: RaceService) {}
+
+    @Get()
+    getAll(): Race[] {
+        return this.raceService.getAll();
+    }
+
+    @Get(':id')
+    getById(@Param('id', new ParseIntPipe()) idPath: number): Race {
+        return this.raceService.getById(idPath);
+    }
+
+    @Post()
+    @HttpCode(HttpStatusCode.Created)
+    create(@Body() raceData: CreateRaceData): Race {
+        return this.raceService.create(raceData);
+    }
+
+    @Delete(':id')
+    delete(@Param('id', new ParseIntPipe()) idPath: number): void {
+        this.raceService.deleteById(idPath);
+    }
 }
