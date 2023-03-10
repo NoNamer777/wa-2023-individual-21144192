3dba71141c28b17503c2b5535a1411a3abd9a566
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-10 23:50:13 +0100

    Add pagination models

---
+++ packages/nest-app/src/app/api/common/models/pagination.model.ts
@@ -0,0 +1,7 @@
+export type SortableAttribute = 'name' | 'size' | 'speed';
+
+export type SortOrder = 'asc' | 'desc';
+
+export const DEFAULT_SORT_ORDER: SortOrder = 'asc';
+
+export const DEFAULT_PAGE_SIZE = 5;
