e580f24a04f06006ad286a41948704e7fc2b6c16
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-13 16:52:05 +0100

    Add environment variables holder

--- packages/nest-app/src/app/app.module.ts
+++ packages/nest-app/src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { RaceModule, TraitModule } from './api';
 import { DatabaseProviderModule } from './configs';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigModuleOptions } from '@nestjs/config/dist/interfaces';
+import { environment } from '../environments/environment';
+
+const configOptions: ConfigModuleOptions = {
+    load: [() => environment],
+};
 
 @Module({
-    imports: [DatabaseProviderModule, RaceModule, TraitModule],
+    imports: [DatabaseProviderModule, ConfigModule.forRoot(configOptions), RaceModule, TraitModule],
+    controllers: [AppController],
 })
 export class AppModule {}
