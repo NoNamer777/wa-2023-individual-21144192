5529f7dc4258872b74ff3ca9a8116c1c8ae5c49c
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-13 17:28:19 +0100

    Run Prettier

--- packages/nest-app/src/app/api/trait/controllers/trait.controller.spec.ts
+++ packages/nest-app/src/app/api/trait/controllers/trait.controller.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TraitController } from './trait.controller';
 
 describe('TraitController', () => {
-  let controller: TraitController;
+    let controller: TraitController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [TraitController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [TraitController],
+        }).compile();
 
-    controller = module.get<TraitController>(TraitController);
-  });
+        controller = module.get<TraitController>(TraitController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
 });
--- packages/nest-app/src/app/api/trait/services/trait.service.spec.ts
+++ packages/nest-app/src/app/api/trait/services/trait.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TraitService } from './trait.service';
 
 describe('TraitService', () => {
-  let service: TraitService;
+    let service: TraitService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TraitService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [TraitService],
+        }).compile();
 
-    service = module.get<TraitService>(TraitService);
-  });
+        service = module.get<TraitService>(TraitService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
