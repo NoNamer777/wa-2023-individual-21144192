a4d5d7292ef7c0bf97766a0fb59cc080e43e9441
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-13 18:52:22 +0100

    Mock dependencies in the tests

--- packages/nest-app/src/app/api/trait/controllers/trait.controller.spec.ts
+++ packages/nest-app/src/app/api/trait/controllers/trait.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TraitController } from './trait.controller';
+import { TraitService } from '../services/trait.service';
 
 describe('TraitController', () => {
     let controller: TraitController;
@@ -7,9 +8,10 @@ describe('TraitController', () => {
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [TraitController],
+            providers: [{ provide: TraitService, useValue: {} }],
         }).compile();
 
-        controller = module.get<TraitController>(TraitController);
+        controller = module.get(TraitController);
     });
 
     it('should be defined', () => {
--- packages/nest-app/src/app/api/trait/services/trait.service.spec.ts
+++ packages/nest-app/src/app/api/trait/services/trait.service.spec.ts
@@ -1,15 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TraitService } from './trait.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Trait } from '../../common/models';
 
 describe('TraitService', () => {
     let service: TraitService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [TraitService],
+            providers: [
+                TraitService,
+                {
+                    provide: getRepositoryToken(Trait),
+                    useValue: {},
+                },
+            ],
         }).compile();
 
-        service = module.get<TraitService>(TraitService);
+        service = module.get(TraitService);
     });
 
     it('should be defined', () => {
