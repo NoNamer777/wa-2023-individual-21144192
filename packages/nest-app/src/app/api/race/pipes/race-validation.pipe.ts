df4c914b5d59c2bf45c3c0885256e949b6ee2f3d
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-14 23:46:30 +0100

    Provide custom error messages

--- packages/nest-app/src/app/api/race/pipes/race-validation.pipe.ts
+++ packages/nest-app/src/app/api/race/pipes/race-validation.pipe.ts
@@ -9,53 +9,126 @@ function imageUrlValidator(value: string, helper: CustomHelpers<string>) {
     value = value.trim();
 
     if (!value.startsWith(base)) {
-        throw new Error(`The image URL doesn't originate from this server.`);
+        return helper.error('serve-url.base');
     }
     value = value.replace(base, '');
     if (!value.match(/^([a-z|A-Z|0-9|\-_ ])*(\.)(png|jpg)$/)) {
-        throw new Error(`The image url doesn't point to a valid image file pattern`);
+        return helper.error('serve-url.image-path');
     }
     return helper.original;
 }
 
-const baseIdAttributeSchema = Joi.number().integer().greater(0);
-const nameAttributeSchema = Joi.string().trim().min(MIN_ENTITY_NAME_LENGTH).required();
-const descriptiveAttributeSchema = Joi.string().trim().min(MIN_ENTITY_DESCRIPTION_LENGTH).optional();
+function sizeValuesString() {
+    let value = '[';
 
-const baseRaceDataSchema = Joi.object().keys({
-    name: nameAttributeSchema,
-    speed: Joi.number().integer().required(),
+    for (const size of SIZE_VALUES) {
+        value += size;
+
+        if (SIZE_VALUES[SIZE_VALUES.length - 1] !== size) {
+            value += ', ';
+        }
+    }
+    return value + ']';
+}
+
+const baseIdAttributeSchema = Joi.number().integer().greater(0);
+const nameAttributeSchema = (entityType: string) =>
+    Joi.string()
+        .trim()
+        .min(MIN_ENTITY_NAME_LENGTH)
+        .required()
+        .messages({
+            'any.required': `Please, provide the name of the ${entityType}.`,
+            'string.base': `Please, provide a valid string as name of the ${entityType}.`,
+            'string.empty': `Please, provide at least ${MIN_ENTITY_NAME_LENGTH} characters for the name of a ${entityType}.`,
+            'string.min': `Please, provide at least ${MIN_ENTITY_NAME_LENGTH} characters for the name of a ${entityType}.`,
+        });
+const descriptiveAttributeSchema = (entityType: string) =>
+    Joi.string()
+        .exist()
+        .allow(null)
+        .trim()
+        .min(MIN_ENTITY_DESCRIPTION_LENGTH)
+        .messages({
+            'any.required': `Please, provide a description of a ${entityType}.`,
+            'string.base': `Please, provide a valid string as description of a ${entityType}.`,
+            'string.empty': `Please, provide at least ${MIN_ENTITY_DESCRIPTION_LENGTH} characters for the description of a ${entityType}.`,
+            'string.min': `Please, provide at least ${MIN_ENTITY_DESCRIPTION_LENGTH} characters for the description of a ${entityType}.`,
+        });
+
+const baseRaceDataSchema = Joi.object({
+    name: nameAttributeSchema('Race'),
+    speed: Joi.number().integer().min(0).required().messages({
+        'any.required': 'Please, provide the speed of the Race.',
+        'number.integer': `Please, provide a whole number equal to or higher than '0' for the speed of the Race.`,
+        'number.min': `Please, provide a whole number equal to or higher than '{#limit}' for the speed of the Race.`,
+        'integer.base': 'Please, provide a whole number for the speed of the Race',
+    }),
     size: Joi.string()
-        .allow(...SIZE_VALUES)
-        .required(),
-    imageUrl: Joi.string().custom(imageUrlValidator).required(),
+        .trim()
+        .valid(...SIZE_VALUES)
+        .required()
+        .messages({
+            'any.required': 'Please, provide the Size of the Race.',
+            'any.only': `Please, provide a valid Size value of the race. ({#valids})`,
+            'string.base': `Please, provide a valid Size value of the race. (${sizeValuesString()})`,
+        }),
+    imageUrl: Joi.string().trim().custom(imageUrlValidator).required().messages({
+        'any.required': `Please, provide the 'imageUrl' of the Race.`,
+        'string.base': `Please, provide a valid url value for the 'imageUrl'.`,
+        'string.empty': `Please, provide a valid url value for the 'imageUrl'.`,
+        'serve-url.base': `The image URL doesn't originate from the server.`,
+        'serve-url.image-path': `The image url path doesn't point to a valid image file pattern`,
+    }),
 });
 
 export const existingRaceDataSchema = baseRaceDataSchema.append({
-    id: baseIdAttributeSchema.required(),
+    id: baseIdAttributeSchema.required().messages({
+        'any.required': 'Please, provide the ID of the Race.',
+        'number.base': 'Please, provide a whole number as the ID of the Race.',
+        'number.greater': `Please, provide a whole number as the ID of the Race which is greater than '{#limit}'.`,
+        'number.integer': `Please, provide a whole number as the ID of the Race which is greater than '0'.`,
+    }),
     traits: Joi.array()
-        .items({
-            trait: {
-                id: baseIdAttributeSchema.required(),
-                name: descriptiveAttributeSchema,
-                description: descriptiveAttributeSchema,
-            },
-            description: descriptiveAttributeSchema,
-        })
-        .required(),
+        .required()
+        .items(
+            Joi.object({
+                trait: Joi.object({
+                    id: baseIdAttributeSchema.required().messages({
+                        'any.required': 'Please, provide the ID of a trait.',
+                        'number.base': 'Please, provide a whole number as the ID of a Trait.',
+                        'number.greater': `Please, provide a whole number as the ID of a Trait which is greater than '{#limit}'.`,
+                        'number.integer': `Please, provide a whole number as the ID of a Trait which is greater than '0'.`,
+                    }),
+                    name: nameAttributeSchema('Trait'),
+                    description: descriptiveAttributeSchema('Trait'),
+                })
+                    .required()
+                    .messages({
+                        'any.required': 'Please, provide the trait of the Racial Trait.',
+                        'object.base': 'Please, provide a valid trait object value for a Racial Trait.',
+                    }),
+                description: descriptiveAttributeSchema('Racial Trait'),
+            })
+        )
+        .messages({
+            'any.required': 'Please, provide the traits of the Race.',
+            'array.base': 'Please, provide a valid list value for the traits of the Race.',
+            'array.min': 'Please, provide at least one trait of the Race.',
+        }),
 });
 
 export const createRaceDataSchema = baseRaceDataSchema.append({
     traits: Joi.array()
+        .required()
         .items({
             trait: {
                 id: baseIdAttributeSchema.optional(),
-                name: descriptiveAttributeSchema,
-                description: descriptiveAttributeSchema,
+                name: nameAttributeSchema('Trait'),
+                description: descriptiveAttributeSchema('Trait'),
             },
-            description: descriptiveAttributeSchema,
-        })
-        .required(),
+            description: descriptiveAttributeSchema('Racial Trait'),
+        }),
 });
 
 @Injectable()
@@ -63,12 +136,13 @@ export class RaceValidationPipe implements PipeTransform {
     constructor(private schema: AnySchema) {}
 
     transform(value: Race): Race {
+        if (!(value instanceof Race)) {
+            return value;
+        }
         const { error } = this.schema.validate(value);
 
         if (error) {
-            throw new BadRequestException({
-                errors: error,
-            });
+            throw new BadRequestException(error.message);
         }
         return value;
     }
