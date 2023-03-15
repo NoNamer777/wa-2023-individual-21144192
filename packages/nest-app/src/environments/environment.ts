a399e9b8fb0565bef731c0d11250bec60094ef5a
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-15 14:25:24 +0100

    Add configuration options for the server

--- packages/nest-app/src/environments/environment.ts
+++ packages/nest-app/src/environments/environment.ts
@@ -1,7 +1,14 @@
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 
+interface ServerEnvironmentValues {
+    host: string;
+    port?: number;
+    secure?: boolean;
+}
+
 interface EnvironmentValues {
     database?: MysqlConnectionOptions;
+    server?: ServerEnvironmentValues;
 }
 
 export const environment: EnvironmentValues = {};
