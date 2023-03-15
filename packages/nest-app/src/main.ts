4ae3cf46adb49730ea5bd95d05b0b0ed3f8725ee
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-15 14:30:09 +0100

    Use config for host and port of server

--- packages/nest-app/src/main.ts
+++ packages/nest-app/src/main.ts
@@ -3,22 +3,27 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app';
 import { setupSwaggerModule } from './app/configs';
+import { environment } from './environments/environment';
+
+const DEFAULT_SERVER_HOSTNAME = 'localhost';
+const DEFAULT_SERVER_PORT = 8080;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    const port = process.env.PORT || 8080;
 
-    app.useGlobalPipes(
-        new ValidationPipe({
-            transform: true,
-        })
-    );
+    const { host, port, secure } = {
+        host: DEFAULT_SERVER_HOSTNAME,
+        port: DEFAULT_SERVER_PORT,
+        secure: false,
+        ...environment.server,
+    };
 
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
     setupSwaggerModule(app);
 
-    await app.listen(port);
+    await app.listen(port, host);
 
-    Logger.log(`Application is running on: http://localhost:${port}/`);
+    Logger.log(`Application is running on: http://${host}:${port}/`);
 }
 
 bootstrap();
