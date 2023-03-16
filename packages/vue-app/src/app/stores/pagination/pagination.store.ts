ef464f487f389f873a6c6195c6eb16100c84dff3
Oscar Wellner <oscar.wellner@gmail.com>
2023-03-16 19:45:02 +0100

    Reimplement pagination store

--- packages/vue-app/src/app/stores/pagination/pagination.store.ts
+++ packages/vue-app/src/app/stores/pagination/pagination.store.ts
@@ -1,5 +1,56 @@
+import { DEFAULT_PAGE_SIZE } from '@dnd-mapp/data';
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+interface Pagination {
+    page: number;
+    pageSize: number;
+    totalPages: number;
+    first: boolean;
+    last: boolean;
+    totalResults: number;
+}
 
 export const usePaginationStore = defineStore('pagination', () => {
-    return {};
+    const pagination = ref<Pagination>({
+        page: 1,
+        pageSize: DEFAULT_PAGE_SIZE,
+        totalPages: 1,
+        first: true,
+        last: true,
+        totalResults: 0,
+    });
+
+    function patchState(value: Partial<Pagination>): void {
+        pagination.value = {
+            ...pagination.value,
+            ...value,
+        };
+    }
+
+    function setPage(page: number): void {
+        pagination.value = { ...pagination.value, page };
+    }
+
+    function setPageSize(pageSize: number): void {
+        pagination.value = { ...pagination.value, pageSize };
+    }
+
+    function setTotalPages(totalPages: number): void {
+        pagination.value = { ...pagination.value, totalPages };
+    }
+
+    function setFirst(first: boolean): void {
+        pagination.value = { ...pagination.value, first };
+    }
+
+    function setLast(last: boolean): void {
+        pagination.value = { ...pagination.value, last };
+    }
+
+    function setTotalResults(totalResults: number): void {
+        pagination.value = { ...pagination.value, totalResults };
+    }
+
+    return { pagination, patchState, setPage, setPageSize, setTotalPages, setFirst, setLast, setTotalResults };
 });
